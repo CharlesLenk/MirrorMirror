4c683083929321291cc968ba433dbd4a49840201
Charles Lenk <lenk.charles@gmail.com>
2022-06-25 20:06:29 -0700

    Combined date and time for new screen

--- js/main.js
+++ js/main.js
@@ -7,43 +7,10 @@ $(document).ready(function () {
     setInterval(function() {
         updateWeather('47.6101,-122.2015', darkSkyApiKey);
     }, 600000);
-
-    loadGreeting();
-    setInterval(loadGreeting, 1800000);
-
-    showBusesForStops();
-    setInterval(showBusesForStops, 60000);
 });
 
 function updateDate() {
-    let date = $('#date');
-    date.text(Intl.DateTimeFormat('en', { weekday: 'long', month: 'long', day: 'numeric' }).format(new Date()));
-    let time = $('#time');
-    time.text(Intl.DateTimeFormat('en', { hour: `numeric`, minute: `numeric`, hour12: true }).format(new Date()));
+    let dateTime = $('#dateTime');
+    dateTime.text(Intl.DateTimeFormat('en', { weekday: 'long', month: 'long', day: 'numeric', hour: `numeric`, minute: `numeric`, hour12: true }).format(new Date()));
 }
 
-let general_greetings = [
-    "You look amazing today.",
-    "It's a beautiful day!",
-    "You're cool!",
-    "Today's a good day.",
-    "You're the fairest of them all!"
-];
-
-function loadGreeting() {
-    var current_greetings = general_greetings.slice();
-    switch((new Date()).getDay()) {
-        case 3:
-            current_greetings.push("Happy Hump Day!"); 
-            break;
-        case 4:
-            current_greetings.push("It's Friday Eve!");
-            break;
-        case 5:
-            current_greetings.push("It's Friday!");
-            break;
-    }
-
-    var index = Math.floor(Math.random() * current_greetings.length);
-    $("#greeting").text(current_greetings[index]);
-}
